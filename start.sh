469204eb01d188d4e3d13f5948652561dfc3dbb2
nfl <nawfalona@gmail.com>
2022-08-07 20:37:05 +0100

    install zcash apt

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ TZ=Africa/Casablanca
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
 apt-get upgrade -y
-apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev
+apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev apt-transport-https gnupg2
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
@@ -13,3 +13,11 @@ sleep 5
 sudo apt-get install -y nano runit
 runsvdir -P /etc/service &
 source $HOME/.bashrc
+wget -qO - https://apt.z.cash/zcash.asc | gpg --import
+gpg --export 3FE63B67F85EA808DE9B880E6DEF3BAF272766C0 | sudo apt-key add -
+echo "deb [arch=amd64] https://apt.z.cash/ buster main" | sudo tee /etc/apt/sources.list.d/zcash.list
+sudo apt-get update
+sudo apt-get install -y zcash
+zcash-fetch-params
+touch ~/.zcash/zcash.conf
+zcashd
