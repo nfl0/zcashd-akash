b7dd1a8cd47dbf0d9bb2a2c33d473690e3d7b192
nfl <nawfalona@gmail.com>
2022-08-07 22:01:11 +0100

    mkdir && touch zcash.conf

--- start.sh
+++ start.sh
@@ -19,5 +19,5 @@ echo "deb [arch=amd64] https://apt.z.cash/ buster main" | sudo tee /etc/apt/sour
 sudo apt-get update
 sudo apt-get install -y zcash
 zcash-fetch-params
-cp /usr/share/doc/zcash/examples/zcash.conf /root/.zcash
+mkdir -p /root/.zcash/ && touch /root/.zcash/zcash.conf
 zcashd
